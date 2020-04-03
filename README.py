c62a2498e4333b0005e6812e13eae962f1f27b11
FSocietyR <48395407+FSocietyR@users.noreply.github.com>
2020-04-03 23:58:11 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,9 +1,6 @@
 import random
+import database_connection as db
 
-import colorama
-from colorama import Fore
-
-# functions
 prov_ = lambda x, y: x > y
 
 accept_ = lambda x: x.get(random.choice(list(x.keys())))  # принимать лекарства
@@ -12,7 +9,7 @@ randomise_ = lambda: random.random()  # возвращает рандомное 
 
 rand_ = lambda x: (int(random.choice(x)) / 100) * randomise_()  # возвращает рандомный кэф из последовательности
 
-random_id = lambda x: random.randrange(0,599)
+random_id = lambda x: random.randrange(0, 599)
 
 
 def creation_of_mortality():
@@ -20,61 +17,6 @@ def creation_of_mortality():
     while mortality > 0.1732241 or mortality < 0.073:
         mortality = randomise_()
     return (mortality)
-
-
-class clear_the_screen:
-    def __repr__(self):
-        import os
-        import time
-        time.sleep(3)
-        os.system('cls')
-        return
-
-class books():
-
-    book_of_id = {}  # словарь с id и координатами
-    book_of_health = {}  # хранятся key = id value = health
-    book_of_rationalities = {}  # хранятся key = id, value = rationality
-    book_of_immune_system = {} # хранятся key = id, value = immune
-    book_of_aliveness = {}
-
-    def zapolnenie_id(C, n, m):
-
-        for i in range(len(matrix)):
-
-            for j in range(len(matrix[i])):
-
-                books.book_of_id[C] = str(n) + ';' + str(m)
-
-                C += 1
-                m += 1
-                if m == 10:
-                    n += 1
-                    m = 0
-                if n == 10:
-                    n = 0
-
-
-    def zapolenie_knig(X):
-        for person in books.book_of_id:
-            X[person] = randomise_()
-
-    def creation_book_of_aliveness():
-
-        for person in books.book_of_id:
-
-            if books.book_of_health[person] > 0:
-                books.book_of_aliveness[person] = 1
-
-            else:
-                books.book_of_aliveness[person] = 'DEAD'
-                books.book_of_health[person] = 'NOT ALIVE, SORRY'
-
-def print_Matrix(matrix):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:2}".format(matrix[i][j]), end=" ")
-        print()
 
 
 def sozdaniye_matrix(matrix):
@@ -93,31 +35,93 @@ def main_proverka(x, y):
     return (x)
 
 
-accept_medicine = lambda health: health * 1.37 + 2 * (accept_(medicine_anti_virus.anti_virus) * randomise_()) - (
-        health * (1 - mortality))
+class books():
+    book_of_id: dict = {}  # словарь с id и координатами
+    book_of_health: dict = {}  # хранятся key = id value = health
+    book_of_rationalities: dict = {}  # хранятся key = id, value = rationality
+    book_of_immune_system: dict = {}  # хранятся key = id, value = immune
+    book_of_recoveries: dict = {}  # key = id | value = chance_of_recoveries
+    book_of_infections: dict = {}
+    book_of_mortality: dict = {}
+    book_of_working_medicine_percentage: dict = {}
+
+    def filling_the_id(id_position: int, oy_coordinate, ox_coordinate):
+
+        for i in range(len(matrix)):
+
+            for j in range(len(matrix[i])):
+
+                books.book_of_id[id_position] = str(oy_coordinate) + ';' + str(ox_coordinate)
+
+                id_position += 1
+                ox_coordinate += 1
+                if ox_coordinate == 10:
+                    oy_coordinate += 1
+                    ox_coordinate = 0
+                if oy_coordinate == 10:
+                    oy_coordinate = 0
+
+    def creeation_of_books(book: dict):
+        for person in books.book_of_id:
+            book[person] = randomise_()
+
+    def creation_of_book_of_chances(self: dict, chance_of_whatever: str) -> None:
+        counter = 0
+        sum = 0
+
+        def what_is_whatever(chance_of_whatever: str) -> float:
+
+            if chance_of_whatever == 'infection':
+                chance_of_whatever: float = 0
+                while (chance_of_whatever / chance_of_infections) < 0.9378765845:
+                    chance_of_whatever = randomise_()
+                return (chance_of_whatever)
+
+            if chance_of_whatever == 'mortality':
+                chance_of_whatever: float = 1.00
+                while (chance_of_whatever / chance_of_mortality_) > 0.75:
+                    chance_of_whatever = randomise_()
+                return (chance_of_whatever)
+
+            if chance_of_whatever == 'recovery':
+                chance_of_whatever: float = 0
+                while (chance_of_whatever / chance_of_recoveries) < chance_of_infections:
+                    chance_of_whatever = randomise_()
+                return (chance_of_whatever)
+
+            if chance_of_whatever == 'medicine':
+
+                def chance_of_working_medicine() -> list:
+                    massive = []
+                    for medicine in medicine_anti_virus.anti_virus:
+                        chance_of_whatever = randomise_()
 
 
-def delta_zdorovia(X):
-    for key, value in X.items():
+                        massive.append(chance_of_whatever)
+                    return massive
+
+                for person in books.book_of_id:
+
+                    books.book_of_working_medicine_percentage[person] = chance_of_working_medicine()
+
+        for person in books.book_of_id:
+
+            if chance_of_whatever != 'medicine':
+                self[person] = what_is_whatever(chance_of_whatever)
+                sum += what_is_whatever(chance_of_whatever)
+                counter += 1
+
+            else:
+                what_is_whatever(chance_of_whatever)
+                break
+
+        # returns average keys in dict
         try:
-            X[key] = accept_medicine(value)
-        except TypeError:
+            return print(sum / counter)
+
+        except ZeroDivisionError:
             pass
 
-def counter():
-    import time
-    from time import sleep
-    counter = 0
-    for i in range(120):
-        delta_zdorovia(books.book_of_health)
-        People.create_main_dictionary_for_class_people()
-        cls = clear_the_screen()
-        cls.__repr__()
-        print('DAY {}'.format(counter))
-
-        print_Matrix(matrix)
-        print(chance_of_infections, chance_of_recoveries, mortality)
-        counter+=1
 
 class medicine_anti_virus:
     anti_virus = {
@@ -133,12 +137,68 @@ class medicine_anti_virus:
         'medicine_10': 0.024,
     }
 
-# variables
+
+class People:
+
+    def __init__(self, id: int, condition: str, immune_system: bool, rationality: float, alive: bool,
+                 chance_of_infection: float, chance_of_recovery: float, chance_of_mortality: float, y_coordinate: int,
+                 x_coordinate: int) -> None:
+        self.id = id
+        self.condition = condition
+        self.immune_system = immune_system
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.rationality = rationality
+        self.chance_of_illnesses = chance_of_infection
+        self.chance_of_healthy = chance_of_recovery
+        self.chance_of_mortality = chance_of_mortality
+        self.alive = alive
+
+    # def checking_the_infection(alive, condition_of_person):
+    #     try:
+    #         if alive != 'DEAD' or alive != 0:
+    #             return condition_of_person > chance_of_infections
+    #         else:
+    #             return 'DEAD'
+    #     except TypeError:
+    #         pass
+    #
+    # def function_of_infection_person(person, condition_of_person, alive, y_coordinate_of_person,
+    #                                  x_coordinate_of_person):
+    #
+    #     if People.checking_the_infection(alive, condition_of_person) == 'DEAD':
+    #
+    #         matrix[y_coordinate_of_person][x_coordinate_of_person] = conditions_of_people.cond_3
+    #         person.alive = 0
+    #
+    #     else:
+    #
+    #         if People.checking_the_infection(alive, condition_of_person) == 1:
+    #
+    #             matrix[y_coordinate_of_person][x_coordinate_of_person] = conditions_of_people.cond_2
+    #
+    #         else:
+    #
+    #             matrix[y_coordinate_of_person][x_coordinate_of_person] = conditions_of_people.cond_1[
+
+    def create_main_dictionary_for_class_people():
+        for person in books.book_of_id:
+            person = People(person, books.book_of_health[person], False, books.book_of_rationalities[person],
+                            True, books.book_of_infections[person],
+                            books.book_of_recoveries[person],
+                            books.book_of_mortality[person],
+                            int(books.book_of_id[person][0]),
+                            int(books.book_of_id[person][-1]))
+            # People.function_of_infection_person(person, person.condition, person.alive, person.y_coordinate,
+            #                                     person.x_coordinate)
+
+
+# constants
 x = 10  # количество людей по оси OY
 y = 10  # количество людей по оси OX
-n = 0  # y-координата
-m = 0  # x-координата
-C = 0  # id
+oy_coordinate = 0  # y-координата
+ox_coordinate = 0  # x-координата
+id_position = 0  # id
 
 
 class conditions_of_people:
@@ -149,84 +209,44 @@ class conditions_of_people:
     cond_5 = '?'  # период инкубации
 
 
-mortality = creation_of_mortality() #до тех пор пока mortality > 0.17.... он будет создавать новое значение
+mortality = creation_of_mortality()  # до тех пор пока mortality > 0.17.... он будет создавать новое значение
 matrix = []
 chance_of_recoveries = (1 - mortality) * randomise_()
 massive_with_numbers = [i for i in range(1, 101)]
 
-# Подключение словарей
-
-
-class People:
-
-
-    def __init__(self, id, condition, rationality, alive, y_coordinate, x_coordinate):
-        self.id = id
-        self.condition = condition
-        self.x_coordinate = x_coordinate
-        self.y_coordinate = y_coordinate
-        self.rationality = rationality
-        self.alive = alive
-        # self.immune_sistem = immune_sistem
-
-
-    def checking_the_infection(alive, condition_of_person):
-        try:
-            if alive != 'DEAD' or alive != 0:
-                return(condition_of_person>chance_of_infections)
-            else:
-                return ('DEAD')
-        except TypeError:
-            pass
-
-    def function_of_infection_person(person, condition_of_person, alive, y_coordinate_of_person, x_coordinate_of_person):
-
-        if People.checking_the_infection(alive, condition_of_person) == 'DEAD':
-            matrix[y_coordinate_of_person][x_coordinate_of_person] = conditions_of_people.cond_3
-            person.alive = 0
-        else:
-            if People.checking_the_infection(alive,condition_of_person) == True:
-                matrix[y_coordinate_of_person][x_coordinate_of_person] = conditions_of_people.cond_2
-            else:
-                matrix[y_coordinate_of_person][x_coordinate_of_person] = conditions_of_people.cond_1
-
-
-    def immune_sistem():
-        pass
-    def create_main_dictionary_for_class_people():
-        for person in books.book_of_id:
-
-            person = People(person,books.book_of_health[person],books.book_of_rationalities[person], books.book_of_aliveness[person], int(books.book_of_id[person][0]), int(books.book_of_id[person][-1]))
-            People.function_of_infection_person(person, person.condition, person.alive, person.y_coordinate, person.x_coordinate)
-
-immunitet = 1
-
-
-
-
-
-
-
 if __name__ == "__main__":
+    chance_of_mortality_ = creation_of_mortality()
+
     chance_of_infections = rand_(massive_with_numbers)
+
     chance_of_recoveries = main_proverka(chance_of_recoveries, chance_of_infections)
+
     main_proverka(chance_of_recoveries, chance_of_infections)
+
     sozdaniye_matrix(matrix)
-    books.zapolnenie_id(C, n, m)
-    books.zapolenie_knig(books.book_of_rationalities)
-    books.zapolenie_knig(books.book_of_health)
-    books.creation_book_of_aliveness()
-    print_Matrix(matrix)
-    print(chance_of_infections, chance_of_recoveries, mortality)
-    cls = clear_the_screen()
-    # print(book_of_id, '\n', book_of_health, '\n', book_of_rationalities)
 
-    # print(book_of_health)
-    counter()
+    books.filling_the_id(id_position, oy_coordinate, ox_coordinate)
 
+    books.creeation_of_books(books.book_of_rationalities)
 
-# TODO: прописать функцию заражения; прописать функцию смертности;прописать функцию излечения; добавить изменение иммунитета с учетом приема лекарств; добавить коэфициент ума;добавить подключение к sqlite3, весы, и псевдо машинное обучение; добавить анимацию
-# TODO: написать функцию появления имунной сиситемы: после того как человек вылечился  random.randrange(0,1) если 1: иммунитет, так же добавить в объект класса был ли он болен (1,2) 1: был, 2 : не был
-# TODO: добавить from tkinter import *; объекты: на каждый cond; анимацию с ними (перестроить весь код на tkinter)
-# TODO: сделать так чтобы если человек умер, то он был мертв и заболеть уже не мог
-# TODO: убрать из __init__() alive т.к. этот параметр будет передаваться в condition
+    books.creeation_of_books(books.book_of_health)
+
+    books.creation_of_book_of_chances(books.book_of_infections, 'infection')
+
+    books.creation_of_book_of_chances(books.book_of_mortality, 'mortality')
+
+    books.creation_of_book_of_chances(books.book_of_recoveries, 'recovery')
+
+    books.creation_of_book_of_chances(books.book_of_working_medicine_percentage, 'medicine')
+
+    # People.create_main_dictionary_for_class_people()
+
+    print(books.book_of_infections)
+
+    print(books.book_of_mortality)
+
+    print(books.book_of_recoveries)
+
+    print(books.book_of_working_medicine_percentage)
+
+    print(books.book_of_id)
