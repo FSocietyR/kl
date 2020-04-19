274e94d71a36f4061c71f282e50714a1319a5b4d
FSocietyR <48395407+FSocietyR@users.noreply.github.com>
2020-04-19 15:34:13 +0300

    Update manager.py

--- manager.py
+++ manager.py
@@ -1,32 +1,88 @@
 import sqlite3 as sql
+import random
+
+# connection to db
 
 conn = sql.connect('Main_DataBase_for_Python_Models.db')
 cursor = conn.cursor()
 
-sql_viruses = "SELECT * FROM viruses "
-cursor.execute(sql_viruses)
+cursor.execute(""" CREATE TABLE IF NOT EXISTS viruses
+                        (name text, chance_of_beeig_ill_average float, chance_of_recovery_average float,
+                        chance_of_mortality_average float )
 
-viruses = {}
-for obj in cursor.fetchall():
-    viruses[obj[0]] = [obj[1], obj[2], obj[3]] """ Получаем dict:
-                                                 key = virus 
-                                                 value = [chance_of_infections, 
-                                                            chance_of_mortality_, 
-                                                            chance_of_recoveries] """
+""")
 
 
-sql_people = "SELECT * FROM people"
-cursor.execute(sql_people)
-for person in cursor.fetchall():
-    print("{id} | {condition} | {health} | {rationality} | {immune_system} | {chance_of_illnesses} | {chance_of_healthy} | {chance_of_mortality} | {medicine} "\
-        .format(id = person[0],
-         condition = person[1],
-          health = person[2],
-           rationality = person[3],
-            immune_system = person[4],
-             chance_of_illnesses = person[5],
-              chance_of_healthy = person[6],
-               chance_of_mortality = person[7],
-                medicine = person[-1]))
+def _Write_Into_The_DataBase_Virus(virus, chance_of_infections, chance_of_mortality_, chance_of_recoveries):
+    Information_About_Viruses = [str(virus),
+
+                                 chance_of_infections,
+
+                                 chance_of_recoveries,
+
+                                 chance_of_mortality_
+
+                                 ]
+
+    cursor.executemany("INSERT INTO viruses values(?, ?, ?, ?)", Information_About_Viruses)
+    conn.commit()
+
+
+cursor.execute(""" CREATE  TABLE IF NOT EXISTS people
+                             (id text, condition text, percent_of_health float, rationality float,
+                                   immune_system bool, chance_of_illnesses float,
+                                   chance_of_healthy float, chance_of_mortality float,
+                                   alive str)
+               """)
+
+
+def _write_into_the_DataBase(person):
+    conn = sql.connect('Main_DataBase_for_Python_Models.db')
+    cursor = conn.cursor()
+
+    passport = [str(person.id),
+                None,
+                person.condition,
+                person.rationality,
+                person.immune_system,
+                person.chance_of_illnesses,
+                person.chance_of_healthy,
+                person.chance_of_mortality,
+                str(person.alive)]
+    # person.medicine
+
+    cursor.executemany("INSERT INTO people values(?, ?, ?, ?, ?, ?, ?, ?, ?)", (passport,))
+    conn.commit()
+    del passport
+    return('END')
+
+
+def request_from_db(id):
+    request_1 = "SELECT condition FROM people where id =?"
+    _request_1 = cursor.execute(request_1, [(str(id))])
+
+    request_2 = "SELECT percent_of_health FROM people where id = ?"
+    _request_2 = cursor.execute(request_2, [(str(id))])
+
+    request_3 = "SELECT chance_of_health FROM people where id = ?"
+    _request_3 = cursor.execute(request_3, [(str(id))])
+
+    if _request_2 > 0.5:
+        if random.random() < _request_3:
+            main_rework = """
+      UPDATE people
+      SET immune_system = int(1)
+      SET condition = 'NOT_ILL_ANYMORE'
+      WHERE id = int(id)"""
+
+            cursor.execute(main_rework)
+            conn.commit()
+
 
 conn.close()
+# TODO: добавить в новом файле *.py выгрузку значений из БД, их изменение, загрузку обратно
+# TODO: добавить в GUI.py получение значений из БД, иконки людей (на все cond_X)
+# TODO: добавить подключение к MatPlotLib и построение графика
+# TODO: сделать для каждого id свой расчет смертности
+# TODO: добавить в *.py изменение лекарств и запись их в БД
+# TODO: move request_from_db to manager
