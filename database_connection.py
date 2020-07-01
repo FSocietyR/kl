9b0aa5c42c69bd029fe899aa7ecd64710a0ee14a
FSocietyR <48395407+FSocietyR@users.noreply.github.com>
2020-07-02 00:25:46 +0300

    Update database_connection.py

--- database_connection.py
+++ database_connection.py
@@ -48,41 +48,38 @@ def _write_into_the_DataBase(person):
                 person.chance_of_illnesses,
                 person.chance_of_healthy,
                 person.chance_of_mortality,
-                str(person.alive)]
+                str(person.alive),
+                0]
     # person.medicine
 
-    cursor.executemany("INSERT INTO people values(?, ?, ?, ?, ?, ?, ?, ?, ?)", (passport,))
+    cursor.executemany("INSERT INTO people values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (passport,))
     conn.commit()
     del passport
     return('END')
 
 
-def request_from_db(id):
-    request_1 = "SELECT condition FROM people where id =?"
-    _request_1 = cursor.execute(request_1, [(str(id))])
-
-    request_2 = "SELECT percent_of_health FROM people where id = ?"
-    _request_2 = cursor.execute(request_2, [(str(id))])
-
-    request_3 = "SELECT chance_of_health FROM people where id = ?"
-    _request_3 = cursor.execute(request_3, [(str(id))])
-
-    if _request_2 > 0.5:
-        if random.random() < _request_3:
-            main_rework = """
-      UPDATE people
-      SET immune_system = int(1)
-      SET condition = 'NOT_ILL_ANYMORE'
-      WHERE id = int(id)"""
-
-            cursor.execute(main_rework)
-            conn.commit()
+# def request_from_db(id):
+#     request_1 = "SELECT condition FROM people where id =?"
+#     _request_1 = cursor.execute(request_1, [(str(id))])
+#
+#     request_2 = "SELECT percent_of_health FROM people where id = ?"
+#     _request_2 = cursor.execute(request_2, [(str(id))])
+#
+#     request_3 = "SELECT chance_of_health FROM people where id = ?"
+#     _request_3 = cursor.execute(request_3, [(str(id))])
+#
+#     if _request_2 > 0.5:
+#         if random.random() < _request_3:
+#             main_rework = """
+#       UPDATE people
+#       SET immune_system = int(1)
+#       SET condition = 'NOT_ILL_ANYMORE'
+#       WHERE id = int(id)"""
+#
+#             cursor.execute(main_rework)
+#             conn.commit()
 
 
 conn.close()
-# TODO: добавить в новом файле *.py выгрузку значений из БД, их изменение, загрузку обратно
-# TODO: добавить в GUI.py получение значений из БД, иконки людей (на все cond_X)
 # TODO: добавить подключение к MatPlotLib и построение графика
-# TODO: сделать для каждого id свой расчет смертности
 # TODO: добавить в *.py изменение лекарств и запись их в БД
-# TODO: move request_from_db to manager
